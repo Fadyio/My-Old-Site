f1e341ee22081774a6fdcbf6a382250a09b29488
fady0 <fadinagh0@gmail.com>
2022-05-31 06:03:38 +0200

    first commi

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,7 +1,7 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
+  title: 'Fady Blog',
+  author: 'Fady nagh',
+  headerTitle: '',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
