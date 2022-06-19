96125218b7f73a70cce82a1dfc274dd5d98a577a
fady0 <fadinagh0@gmail.com>
2022-06-19 08:14:31 +0200

    test website

---
+++ node_modules/mdx-bundler/dist/dirname-messed-up.cjs
@@ -0,0 +1,6 @@
+"use strict";
+
+// __dirname isn't supported in ESM files
+// we could use import.meta, but that may not be supported by whatever bundler
+// folks may be using, so we'll just go with this...
+module.exports = !__dirname.includes('mdx-bundler');
