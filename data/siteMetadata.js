b4a4eca5ef65efe0b7390ccacb83362a2071ab85
fady0 <fadinagh0@gmail.com>
2022-06-23 10:27:11 +0200

    change the logo of my site

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -21,9 +21,6 @@ const siteMetadata = {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
     // supports plausible, simpleAnalytics, umami or googleAnalytics
-    plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
-    simpleAnalytics: false, // true or false
-    umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
     googleAnalyticsId: 'G-1ZN2JNL2H3', // e.g. UA-000000-2 or G-XXXXXXX
   },
   newsletter: {
