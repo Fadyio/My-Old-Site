cb8a299cf66249becbead7ea2a1ce5ff6bf2018c
fady0 <fadinagh0@gmail.com>
2022-07-07 17:35:31 +0200

    add cdn.jsdelivr.net to csp

--- next.config.js
+++ next.config.js
@@ -6,11 +6,11 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 const ContentSecurityPolicy = `
   default-src 'self';
   script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app www.googletagmanager.com  www.google-analytics.com; 
-  style-src 'self' 'unsafe-inline';
+  style-src 'self' 'unsafe-inline' cdn.jsdelivr.net;
   img-src * blob: data:;
   media-src 'none';
   connect-src *;
-  font-src 'self';
+  font-src 'self' cdn.jsdelivr.net;
   frame-src giscus.app
 `
 
