1ddb74b4706db674a0333ea9efa90acec27b0ca3
fady0 <fadinagh0@gmail.com>
2022-07-08 17:15:10 +0200

    add

--- pages/index.js
+++ pages/index.js
@@ -74,10 +74,10 @@ export default function Home({ posts }) {
                           viewBox="0 0 24 24"
                           fill="none"
                           stroke="currentColor"
-                          stroke-width="2"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          class="feather feather-github"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                        strokeLinecap="round"
+                          className="feather feather-github"
                         >
                           <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
                         </svg>
@@ -88,10 +88,10 @@ export default function Home({ posts }) {
                           viewBox="0 0 24 24"
                           fill="none"
                           stroke="currentColor"
-                          stroke-width="2"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          class="feather feather-external-link"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinecap="round"
+                          className="feather feather-external-link"
                         >
                           <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
                           <polyline points="15 3 21 3 21 9"></polyline>
