947a74897740c727b234b4dd88df11c6df9b0690
Fadyio <me@fadyio.com>
2022-07-09 12:06:32 +0200

    change the post layout

--- layouts/PostLayout.js
+++ layouts/PostLayout.js
@@ -23,8 +23,7 @@ const editUrl = (fileName) => `${siteMetadata.siteRepo}/blob/master/data/blog/${
 
 export default function PostSimple({ frontMatter, authorDetails, next, prev, children }) {
   const { slug, fileName, date, title, summary, readingTime } = frontMatter
-    const postUrl = `${siteMetadata.siteUrl}/blog/${slug}`
-
+  const postUrl = `${siteMetadata.siteUrl}/blog/${slug}`
 
   return (
     <SectionContainer>
@@ -68,98 +67,98 @@ export default function PostSimple({ frontMatter, authorDetails, next, prev, chi
             <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
               <div className="prose max-w-none pt-10 pb-8 dark:prose-dark">{children}</div>
             </div>
-               <div className="flex items-center">
-                  <TwitterShareButton
-                    url={postUrl}
-                    title={title}
-                    via={siteMetadata.socialAccount.twitter}
-                    className="mr-2 flex items-center overflow-hidden rounded-full !bg-[#1da1f2] !p-1.5 hover:scale-110"
-                  >
-                    <SocialIcon
-                      network="twitter"
-                      style={{ height: 25, width: 25 }}
-                      fgColor="#1da1f2"
-                      bgColor="#fff"
-                    />
-                  </TwitterShareButton>
-                  <FacebookShareButton
-                    url={postUrl}
-                    quote={title}
-                    className="mr-2 flex items-center overflow-hidden rounded-full !bg-[#1877f2] !p-1.5 hover:scale-110"
-                  >
-                    <SocialIcon
-                      network="facebook"
-                      style={{ height: 25, width: 25 }}
-                      fgColor="#1877f2"
-                      bgColor="#fff"
-                    />
-                  </FacebookShareButton>
-                  <EmailShareButton
-                    body={'Check out this blog'}
-                    subject={title}
-                    separator=" : "
-                    url={postUrl}
-                    className="mr-2 flex items-center overflow-hidden rounded-full !bg-[#7f7f7f] !p-1.5 hover:scale-110"
-                  >
-                    <SocialIcon
-                      network="email"
-                      style={{ height: 25, width: 25 }}
-                      fgColor="#7f7f7f"
-                      bgColor="#fff"
-                    />
-                  </EmailShareButton>
-                  <LinkedinShareButton
-                    summary={'Check out this blog'}
-                    title={title}
-                    source={siteMetadata.siteUrl}
-                    url={postUrl}
-                    className="mr-2 flex items-center overflow-hidden rounded-full !bg-[#027fb1] !p-1.5 hover:scale-110"
-                  >
-                    <SocialIcon
-                      network="linkedin"
-                      style={{ height: 25, width: 25 }}
-                      fgColor="#027fb1"
-                      bgColor="#fff"
-                    />
-                  </LinkedinShareButton>
-                  <RedditShareButton
-                    title={title}
-                    url={postUrl}
-                    className="mr-2 flex items-center overflow-hidden rounded-full !bg-[#ff4500] !p-1.5 hover:scale-110"
-                  >
-                    <SocialIcon
-                      network="reddit"
-                      style={{ height: 25, width: 25 }}
-                      fgColor="#ff4500"
-                      bgColor="#fff"
-                    />
-                  </RedditShareButton>
-                <TelegramShareButton
-                    url={postUrl}
-                    quote={title}
-                    className="mr-2 flex items-center overflow-hidden rounded-full !bg-[#1877f2] !p-1.5 hover:scale-110"
-                  >
-                    <SocialIcon
-                      network="telegram"
-                      style={{ height: 25, width: 25 }}
-                      fgColor="#1877f2"
-                      bgColor="#fff"
-                    />
-                  </TelegramShareButton>
-                  <WhatsappShareButton
-                    title={title}
-                    separator={' : '}
-                    url={postUrl}
-                    className="mr-2 flex items-center overflow-hidden rounded-full !bg-[#25D366] !p-1.5 hover:scale-110"
-                  >
-                    <SocialIcon
-                      network="whatsapp"
-                      style={{ height: 25, width: 25 }}
-                      fgColor="#25D366"
-                      bgColor="#fff"
-                    />
-                  </WhatsappShareButton>
-                </div>
+            <div className="flex items-center">
+              <TwitterShareButton
+                url={postUrl}
+                title={title}
+                via={siteMetadata.socialAccount.twitter}
+                className="mr-2 flex items-center overflow-hidden rounded-full !bg-[#1da1f2] !p-1.5 hover:scale-110"
+              >
+                <SocialIcon
+                  network="twitter"
+                  style={{ height: 25, width: 25 }}
+                  fgColor="#1da1f2"
+                  bgColor="#fff"
+                />
+              </TwitterShareButton>
+              <FacebookShareButton
+                url={postUrl}
+                quote={title}
+                className="mr-2 flex items-center overflow-hidden rounded-full !bg-[#1877f2] !p-1.5 hover:scale-110"
+              >
+                <SocialIcon
+                  network="facebook"
+                  style={{ height: 25, width: 25 }}
+                  fgColor="#1877f2"
+                  bgColor="#fff"
+                />
+              </FacebookShareButton>
+              <EmailShareButton
+                body={'Check out this blog'}
+                subject={title}
+                separator=" : "
+                url={postUrl}
+                className="mr-2 flex items-center overflow-hidden rounded-full !bg-[#7f7f7f] !p-1.5 hover:scale-110"
+              >
+                <SocialIcon
+                  network="email"
+                  style={{ height: 25, width: 25 }}
+                  fgColor="#7f7f7f"
+                  bgColor="#fff"
+                />
+              </EmailShareButton>
+              <LinkedinShareButton
+                summary={'Check out this blog'}
+                title={title}
+                source={siteMetadata.siteUrl}
+                url={postUrl}
+                className="mr-2 flex items-center overflow-hidden rounded-full !bg-[#027fb1] !p-1.5 hover:scale-110"
+              >
+                <SocialIcon
+                  network="linkedin"
+                  style={{ height: 25, width: 25 }}
+                  fgColor="#027fb1"
+                  bgColor="#fff"
+                />
+              </LinkedinShareButton>
+              <RedditShareButton
+                title={title}
+                url={postUrl}
+                className="mr-2 flex items-center overflow-hidden rounded-full !bg-[#ff4500] !p-1.5 hover:scale-110"
+              >
+                <SocialIcon
+                  network="reddit"
+                  style={{ height: 25, width: 25 }}
+                  fgColor="#ff4500"
+                  bgColor="#fff"
+                />
+              </RedditShareButton>
+              <TelegramShareButton
+                url={postUrl}
+                quote={title}
+                className="mr-2 flex items-center overflow-hidden rounded-full !bg-[#1877f2] !p-1.5 hover:scale-110"
+              >
+                <SocialIcon
+                  network="telegram"
+                  style={{ height: 25, width: 25 }}
+                  fgColor="#1877f2"
+                  bgColor="#fff"
+                />
+              </TelegramShareButton>
+              <WhatsappShareButton
+                title={title}
+                separator={' : '}
+                url={postUrl}
+                className="mr-2 flex items-center overflow-hidden rounded-full !bg-[#25D366] !p-1.5 hover:scale-110"
+              >
+                <SocialIcon
+                  network="whatsapp"
+                  style={{ height: 25, width: 25 }}
+                  fgColor="#25D366"
+                  bgColor="#fff"
+                />
+              </WhatsappShareButton>
+            </div>
             <Comments frontMatter={frontMatter} />
             <footer>
               <div className="flex flex-col text-sm font-medium sm:flex-row sm:justify-between sm:text-base">
