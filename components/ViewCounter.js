0f1c41a9d7189c7e5b046834b69f1d3a124426d1
Fadyio <me@fadyio.com>
2022-07-09 16:27:25 +0200

    add my github

--- components/ViewCounter.js
+++ components/ViewCounter.js
@@ -1,32 +1,19 @@
-import { useState, useEffect } from 'react'
+import { useEffect } from 'react'
+import useSWR from 'swr'
+import fetcher from 'lib/fetcher'
 
 export default function ViewCounter({ slug, className, blogPage = false }) {
-  const [views, setViews] = useState(0)
+  let { data } = useSWR(`/api/views/${slug}`, fetcher)
+  let views = new Number(data?.total)
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(`/api/views/${slug}`)
-        const json = await response.json()
-        setViews(Number(json?.total))
-      } catch (error) {
-        console.log('error', error)
-      }
-    }
-
-    fetchData()
-  }, [slug])
-
-  useEffect(() => {
-    const registerView = () =>
+    let registerView = () =>
       fetch(`/api/views/${slug}`, {
         method: 'POST',
       })
-
     if (blogPage) {
       registerView()
     }
   }, [blogPage, slug])
-
   return <span className={className}>{`${views > 0 ? views.toLocaleString() : '–––'} Views`}</span>
 }
