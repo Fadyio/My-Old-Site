83e6e472823ecb26785b01355f5fef2873bcfe4d
Fadyio <me@fadyio.com>
2022-07-09 14:10:03 +0200

    change the index layout

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,10 @@ import siteMetadata from '@/data/siteMetadata'
 import { getAllFilesFrontMatter } from '@/lib/mdx'
 import formatDate from '@/lib/utils/formatDate'
 import { RoughNotation } from 'react-rough-notation'
+import ViewCounter from '@/components/ViewCounter'
+import { useRouter } from 'next/router'
 import NewsletterForm from '@/components/NewsletterForm'
-import { motion } from 'framer-motion'
+
 const MAX_DISPLAY = 3
 
 export async function getStaticProps() {
@@ -16,19 +18,60 @@ export async function getStaticProps() {
 }
 
 export default function Home({ posts }) {
+  const router = useRouter()
+
   return (
     <>
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
-      <div>
-        <div className="mb-12 flex flex-col items-center gap-x-12 xl:flex-row">
-          <div className="pt-6">
-            <h1 className="pb-6 text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-              Hi, I am{' '}
-              <span className="text-primary-color-500 dark:text-primary-color-dark-500">Fady</span>
-            </h1>
-            <h2 className="prose text-lg text-gray-600 dark:text-gray-300">
-              {`Welcome to ${siteMetadata.description}. I’m a software developer. and I have bachelor degree in information technology. I have one main opinion about programming and computer science, which is that deeply understanding the underlying systems you use is essential if you want to be able to solve hard problems  `}
+      <div className="space-y-12">
+        <div className="flex flex-col items-center gap-x-12 xl:flex-row">
+          <div className="space-y-6 text-slate-600 dark:text-slate-300">
+            <div className="h-content sm:h-content-sm flex flex-col justify-around">
+              <h1 className="sm:text-8.5xl tracking-tightest my-28 select-none text-center text-6xl font-extrabold leading-none sm:my-10">
+                <span
+                  data-content="Blog."
+                  className="relative block before:absolute before:top-0 before:bottom-0 before:left-0 before:block before:w-full before:animate-gradient-background-1 before:px-2 before:text-center before:text-black before:content-[attr(data-content)] dark:before:text-white dark:before:content-[attr(data-content)]"
+                >
+                  <span className="animate-gradient-foreground-1 bg-gradient-to-br from-gradient-1-start to-gradient-1-end bg-clip-text px-2 text-transparent">
+                    Blog.
+                  </span>
+                </span>
+                <span
+                  data-content="Coding."
+                  className="relative block before:absolute before:top-0 before:bottom-0 before:left-0 before:block before:w-full before:animate-gradient-background-2 before:px-2 before:text-center before:text-black before:content-[attr(data-content)] dark:before:text-white dark:before:content-[attr(data-content)]"
+                >
+                  <span className="animate-gradient-foreground-2 bg-gradient-to-br from-gradient-2-start to-gradient-2-end bg-clip-text px-2 text-transparent">
+                    Coding.
+                  </span>
+                </span>
+                <span
+                  data-content="Portfolio."
+                  className="relative block before:absolute before:top-0 before:bottom-0 before:left-0 before:block before:w-full before:animate-gradient-background-3 before:px-2 before:text-center before:text-black before:content-[attr(data-content)] dark:before:text-white dark:before:content-[attr(data-content)]"
+                >
+                  <span className="animate-gradient-foreground-3 bg-gradient-to-br from-gradient-3-start to-gradient-3-end bg-clip-text px-2 text-transparent">
+                    Portfolio.
+                  </span>
+                </span>
+              </h1>
+            </div>
+
+            <h2 className="prose text-gray-800 dark:text-gray-100 space-x-2 text-lg">
+              <span>
+                Welcome to {siteMetadata.description}. I am a Software Engineer passionate about
+                frontend tech, building pipelines and automation. In my free time, I like developing
+              </span>
+              <Link href="/projects" className="special-underline no-underline">
+                side projects
+              </Link>
+              <span>and</span>
+              <Link href="/blog" className="special-underline no-underline">
+                blogging
+              </Link>
+              <span>about them.</span>
             </h2>
+            <p className="text-lg leading-7">
+              This is my place for thoughts, reflections & everything in between. Have a good read!
+            </p>
           </div>
           <div className="flex items-center justify-center">
             <div className="grid grid-cols-1 grid-rows-3 gap-8 py-12">
@@ -129,71 +172,102 @@ export default function Home({ posts }) {
                   </Link>
                 </div>
               </div>
+              <div className="my-2 grid items-start gap-8">
+                <div className="group relative">
+                  <div className="animate-tilt absolute -inset-0.5 rounded-lg bg-gradient-to-r from-pink-600 to-purple-600 opacity-50 blur transition duration-1000 group-hover:opacity-100 group-hover:duration-200"></div>
+                  <Link href="/about">
+                    <span className="relative flex items-center divide-x divide-gray-600 rounded-lg bg-white px-7 py-4 leading-none dark:bg-black">
+                      <span className="flex items-center space-x-5">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-6 w-6 -rotate-6 text-pink-600"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"
+                          />
+                        </svg>
+                        <span className="pr-6 text-gray-900 dark:text-gray-100">Read my story</span>
+                      </span>
+                      <span className="pl-6 text-indigo-400 transition duration-200 group-hover:text-gray-900 dark:group-hover:text-gray-100">
+                        Website&nbsp;&rarr;
+                      </span>
+                    </span>
+                  </Link>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-        <h2 className="flex pb-6 text-3xl font-extrabold tracking-tight text-gray-900 dark:text-gray-100 sm:text-3xl md:text-5xl">
-          Latest
-        </h2>
-        <ul>
-          {!posts.length && 'No posts found.'}
-          {posts.slice(0, MAX_DISPLAY).map((frontMatter) => {
-            const { slug, date, title, summary, tags } = frontMatter
-            return (
-              <motion.li key={slug} whileHover={{ scale: 1.03 }}>
-                <Link
-                  href={`/blog/${slug}`}
+        <div>
+          <h2 className="flex pb-6 text-3xl font-extrabold tracking-tight text-gray-900 dark:text-gray-100 sm:text-3xl md:text-5xl">
+            Latest
+          </h2>
+          <hr className="border-gray-200 dark:border-gray-700" />
+          <ul className="pt-6">
+            {!posts.length && 'No posts found.'}
+            {posts.slice(0, MAX_DISPLAY).map((frontMatter) => {
+              const { slug, date, title, summary, tags } = frontMatter
+              return (
+                <li
+                  onClick={() => router.push(`/blog/${slug}`)}
                   key={slug}
-                  className="group flex bg-transparent bg-opacity-20 px-2 transition duration-100 hover:rounded-xl hover:bg-gray-100 dark:hover:bg-gray-800"
+                  className="group flex cursor-pointer bg-transparent bg-opacity-20 px-2 transition duration-100 hover:scale-105 hover:rounded-xl hover:bg-gray-100 dark:hover:bg-gray-800"
                 >
-                  <li className="py-6 px-4">
-                    <article>
-                      <div className="space-y-2 bg-transparent bg-opacity-20 p-2 transition duration-200 hover:rounded-xl hover:bg-gray-100 dark:hover:bg-gray-800 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
-                        <dl>
-                          <dt className="sr-only">Published on</dt>
-                          <dd className="text-base font-medium leading-6 text-gray-500 group-hover:text-primary-500 dark:text-gray-400">
-                            <time dateTime={date}>{formatDate(date)}</time>
-                          </dd>
-                        </dl>
-                        <div className="space-y-5 xl:col-span-3">
-                          <div className="space-y-6">
-                            <div>
-                              <h2 className="text-2xl font-bold leading-8 tracking-tight">
-                                <Link
-                                  href={`/blog/${slug}`}
-                                  className="text-gray-900 transition duration-500 ease-in-out hover:text-primary-500 dark:text-gray-100 dark:hover:text-primary-500"
-                                >
-                                  {title}
-                                </Link>
-                              </h2>
-                              <div className="flex flex-wrap">
-                                {tags.map((tag) => (
-                                  <Tag key={tag} text={tag} />
-                                ))}
-                              </div>
-                            </div>
-                            <div className="prose max-w-none text-gray-500 dark:text-gray-400">
-                              {summary}
-                            </div>
-                          </div>
-                          <div className="text-base font-medium leading-6">
+                  <article className="py-8 px-4 ">
+                    <div className="space-y-2 bg-transparent bg-opacity-20 p-2 transition duration-200 hover:rounded-xl hover:bg-gray-100 dark:hover:bg-gray-800 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
+                      <dl>
+                        <dt className="sr-only">Published on</dt>
+                        <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                          <time dateTime={date}>{formatDate(date)}</time>
+                        </dd>
+                      </dl>
+                      <div className="space-y-5 xl:col-span-3">
+                        <div className="space-y-1">
+                          <div className="flex flex-col justify-between md:flex-row">
                             <Link
                               href={`/blog/${slug}`}
-                              className="mr-3 rounded-lg border border-primary-500 py-1 px-3 text-sm font-medium uppercase text-primary-500 transition duration-500 ease-in-out hover:bg-primary-500 hover:text-gray-100 dark:hover:text-gray-900"
-                              aria-label={`Read "${title}"`}
+                              className="text-gray-900 transition duration-500 ease-in-out hover:text-primary-500 dark:text-gray-100 dark:hover:text-primary-500"
                             >
-                              Read more &rarr;
+                              <h2 className="text-2xl font-bold leading-8 tracking-tight">
+                                {title}
+                              </h2>
                             </Link>
+                            <p className="mt-1 mb-4 w-32 text-left text-gray-500 md:mb-0 md:text-right">
+                              <ViewCounter slug={slug} blogPage={false} />
+                            </p>
+                          </div>
+                          <div className="flex flex-wrap">
+                            {tags.map((tag) => (
+                              <Tag key={tag} text={tag} />
+                            ))}
+                          </div>
+                          <div className="prose max-w-none pt-5 text-gray-500 dark:text-gray-400">
+                            {summary}
                           </div>
                         </div>
+                        {/* <div className="text-base font-medium leading-6">
+                          <Link
+                            href={`/blog/${slug}`}
+                            className="mt-2 mr-3 rounded-lg border border-primary-500 py-1 px-3 text-sm font-medium uppercase text-primary-500 transition duration-500 ease-in-out hover:bg-primary-500 hover:text-gray-100 dark:hover:text-gray-900"
+                            aria-label={`Read "${title}"`}
+                          >
+                            Read more &rarr;
+                          </Link>
+                        </div> */}
                       </div>
-                    </article>
-                  </li>
-                </Link>
-              </motion.li>
-            )
-          })}
-        </ul>
+                    </div>
+                  </article>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
       </div>
       {posts.length > MAX_DISPLAY && (
         <div className="flex justify-end text-base font-medium leading-6">
@@ -207,7 +281,7 @@ export default function Home({ posts }) {
         </div>
       )}
       {siteMetadata.newsletter.provider !== '' && (
-        <div className="flex items-center justify-center pt-4">
+        <div className="flex items-center justify-center pt-7 ">
           <NewsletterForm />
         </div>
       )}
