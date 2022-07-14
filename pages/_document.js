d689f3b9c95143d8409cc49cffc95ff1d238c126
Fady Nagh <fady@fadyio.com>
2022-07-14 17:18:25 +0200

    update the pages

--- pages/_document.js
+++ pages/_document.js
@@ -104,11 +104,10 @@ class MyDocument extends Document {
           />
           <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-           <link
+          <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
             rel="stylesheet"
           />
-
         </Head>
         <body className="bg-white text-black antialiased dark:bg-background-color dark:text-white">
           <Main />
